b53f06292c136412cb4509ab823bb774ceead41a
panda1835 <lehoangphuc1820@gmail.com>
2024-06-03 13:07:24 +0700

    add new admin views

--- digitizing_vietnam/admin.py
+++ digitizing_vietnam/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from .models import Collection, Document, Blog, OCR, BlogType
-# Register your models here.
-admin.site.register(Collection)
+from .models import *
+
+class OnlineResourceCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'description', 'presentation_order')
+    sortable_by = ('presentation_order',)
+    ordering = ('presentation_order',)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('collection_id', 'title', 'date_created', 'date_updated', 'presentation_order')
+    sortable_by = ('presentation_order',)
+    ordering = ('presentation_order',)
+
+    # Register your models here.
+admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Document)
 admin.site.register(BlogType)
 admin.site.register(Blog)
 admin.site.register(OCR)
+admin.site.register(OnlineResourceCategory, OnlineResourceCategoryAdmin)
+admin.site.register(OnlineResource)
